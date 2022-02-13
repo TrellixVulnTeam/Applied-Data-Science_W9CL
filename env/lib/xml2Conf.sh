cac76811d599dfe6ef1040543eb6881688fd6664
varmor <morevarun4004@gmail.com>
2022-02-13 23:41:40 +0530

    setting up env

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/varun/Documents/Beta/Ml-Course/env/lib"
+XML2_LIBS="-lxml2 -L/home/varun/Documents/Beta/Ml-Course/env/lib -lz -L/home/varun/Documents/Beta/Ml-Course/env/lib -llzma -lpthread -L/home/varun/Documents/Beta/Ml-Course/env/lib -L/home/varun/Documents/Beta/Ml-Course/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/varun/Documents/Beta/Ml-Course/env/include/libxml2 -I/home/varun/Documents/Beta/Ml-Course/env/include"
+MODULE_VERSION="xml2-2.9.12"
+
